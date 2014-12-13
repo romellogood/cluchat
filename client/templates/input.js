b153f1a02bf19f67f3139a65a7197101fcb1adbf
Romello Goodman <romellogood@gmail.com>
2014-12-12 23:11:07 -0800

    First Commit

---
+++ client/templates/input.js
@@ -0,0 +1,20 @@
+Template.input.events = {
+  'keydown input#message' : function (event) {
+    if(event.which == 13) { // 13 is the enter key event
+      var name = Meteor.user().profile.name;
+      name = name.split(' ').slice(0, -1).join(' ');
+      var message = document.getElementById('message');
+
+      if(message.value != '') {
+        Messages.insert({
+          name: name,
+          message: message.value,
+          time: Date.now(),
+        });
+
+        document.getElementById('message').value = '';
+        message.value = '';
+      }
+    }
+  }
+}
---
+++ server/fixtures.js
@@ -0,0 +1,17 @@
+//Plug in results during development
+/*
+if (Messages.find().count() === 0) {
+	Messages.insert({
+    name: "John",
+    message: "Hello Joe, how are you?"
+  });
+  Messages.insert({
+    name: "Joe",
+    message: "I'm fine and you?"
+	});
+  Messages.insert({
+    name: "John",
+    message: "Excellent, thank you for asking!"
+	});
+}
+*/
